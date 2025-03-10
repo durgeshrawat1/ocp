8748e72b4bdbd7446715399f1fbf3c054e3d7390
durgeshrawat1 <durgesh.rawat2011@gmail.com>
2025-03-10 10:03:12 -0500

    Create tailwind.config.js

---
+++ node/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'chat-bg': '#1a1b1e',
+        'sidebar-bg': '#2d2d2d',
+        'input-bg': '#3a3b3c'
+      }
+    },
+  },
+  plugins: [],
+}
+
+
